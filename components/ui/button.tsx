0bcb02c13261fb9995622df49fede535e2b78bd1
promptlabo <maogawa.0101@gmail.com>
2025-05-15 02:12:14 +0900

    Create button.tsx

---
+++ components/ui/button.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Button = ({ children, ...props }) => (
+  <button className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700" {...props}>
+    {children}
+  </button>
+);
